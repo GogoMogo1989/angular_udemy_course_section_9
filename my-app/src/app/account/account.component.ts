86f9e7b3fd80641fbc4fb8bd9334bdefb7ceb6d7
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-10-11 11:09:43 +0200

    20231011

--- my-app/src/app/account/account.component.ts
+++ my-app/src/app/account/account.component.ts
@@ -1,22 +1,23 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: { name: string; status: string; } | undefined;
   @Input() id: number | undefined;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private loggingService: LoggingService){}
 
   onSetTo(status: string) {
     if (typeof this.id === 'number' && typeof status === 'string') {
       this.statusChanged.emit({ id: this.id, newStatus: status });
-      console.log('A server status changed, new status: ' + status);
-    } else {
-      console.error('Invalid id or status');
-    }
+      this.loggingService.logStatusChange(status)
+    } 
   }
 }
